ce80cdbcb16ae0e2a1a7caae30a134967eac02a9
Paul Roberto Messina <paul.messina@globant.com>
2020-11-30 18:29:34 -0300

    Ajuste para arreglar el mapa

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(45.579591,9.164906); 
+  var myCenter = new google.maps.LatLng(-42.067589662590514,-71.59613902855806); 
   var mapOptions = {center: myCenter, zoom: 13};
   var map = new google.maps.Map(mapCanvas,mapOptions);
   var marker = new google.maps.Marker({
